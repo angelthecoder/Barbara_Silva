56111ae4649a3a702dcfaa7b009d19c69dc65118
Heinrich <heinrich@cherrybomb.com.mx>
2017-10-19 12:52:07 -0500

    Ajusta campo en backend para URL de youtube

--- includes/social-bar.php
+++ includes/social-bar.php
@@ -1,10 +1,10 @@
 <section class="bs_social_bar">
 	<div class="bs_container wk-flex-item wk-d-flex-justify-end wk-m-flex-justify-center">
 		<div class="bs_icons">
-			<a class="bs_icon" href="http://instagram.com/<?php the_field( 'bs_instagram', 'option' ); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/icon-instagram.svg"></a>
-			<a class="bs_icon" href="http://facebook.com/<?php the_field( 'bs_facebook', 'option' ); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/icon-facebook.svg"></a>
-			<a class="bs_icon" href="http://twitter.com/<?php the_field( 'bs_twitter', 'option' ); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/icon-twitter.svg"></a>
-			<a class="bs_icon" href="http://pinterest.com/<?php the_field( 'bs_pinterest', 'option' ); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/icon-youtube.svg"></a>
+			<a class="bs_icon" target="_blank" href="http://instagram.com/<?php the_field( 'bs_instagram', 'option' ); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/icon-instagram.svg"></a>
+			<a class="bs_icon" target="_blank" href="http://facebook.com/<?php the_field( 'bs_facebook', 'option' ); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/icon-facebook.svg"></a>
+			<a class="bs_icon" target="_blank" href="http://twitter.com/<?php the_field( 'bs_twitter', 'option' ); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/icon-twitter.svg"></a>
+			<a class="bs_icon" target="_blank" href="<?php the_field( 'bs_pinterest', 'option' ); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/icon-youtube.svg"></a>
 
 <!-- 			<a class="bs_icon" href="#"><span class="fa fa-instagram"></span></a>
 			<a class="bs_icon" href="#"><span class="fa fa-facebook-square"></span></a>
